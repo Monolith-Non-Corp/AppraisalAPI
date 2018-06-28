a6d36f0a727bc7960334591bf5a7f9f5a9362db1
ArekkuusuJerii <edgargcx99@gmail.com>
2018-06-28 02:03:47 -0500

    Allow Credentials header

--- src/main/java/com/pi/appraisal/component/CorsFilter.java
+++ src/main/java/com/pi/appraisal/component/CorsFilter.java
@@ -17,7 +17,7 @@ public class CorsFilter extends OncePerRequestFilter {
 									FilterChain filterChain) throws ServletException, IOException {
 		response.addHeader("Access-Control-Allow-Origin", "*");
 		response.addHeader("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS");
-		response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+		response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Credentials, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 		response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
 		response.addHeader("Access-Control-Allow-Credentials", "true");
 		filterChain.doFilter(request, response);
